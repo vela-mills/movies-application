44578421fd85c4c7e19163e6e2eea9a462cc1703
Bridget Mills <bridgetsmills@gmail.com>
2019-09-11 16:53:47 -0500

    worked on adding functions to api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,19 @@
-module.exports = {
-  getMovies: () => {
+const getMovies = () => {
     return fetch('/api/movies')
-      .then(response => response.json());
-  }
-};
+        .then(response => response.json());
+}
+const addMovie = () => {
+    return fetch('/api/movies')
+        .then(response => response.json());
+}
+const deleteMovie = () => {
+    return fetch('/api/movies')
+        .then(response => response.json());
+}
+const updateMovie = () => {
+    return fetch('/api/movies')
+        .then(response => response.json());
+}
+
+
+module.exports = {getMovies, addMovie, deleteMovie, updateMovie};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 /**
  * es6 modules and imports
  */
-import sayHello from './hello';
-sayHello('World');
+// import sayHello from './hello';
+// sayHello('World');
 
 /**
  * require style imports
@@ -11,8 +11,10 @@ const {getMovies} = require('./api.js');
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
+  let innerHTML = "<ul>";
   movies.forEach(({title, rating, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
+
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')

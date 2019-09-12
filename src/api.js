3c04e26ee39d33ada0f5f96dc95d846174e8aa61
Gloria Laura Vela <gloria.laura.vela@gmail.com>
2019-09-12 15:56:09 -0500

    add the update and delete functionality

--- src/api.js
+++ src/api.js
@@ -7,10 +7,6 @@ const addMovie = (e) => {
     let rating = document.getElementById('rating').value;
     let title = document.getElementById('movie-name').value;
     // Get the last id
-    //let newId = document.getElementById('lastID').value;
-    //newId = parseInt(newId) + 1;
-    //document.getElementById('lastID').innerText = newId;
-    //let id = newId;
     let id = 0;
     fetch('/api/movies')
         .then(response => response.json())
@@ -25,7 +21,7 @@ const addMovie = (e) => {
             id = idArray.sort()[idArray.length - 1];
 
         })
-    // Create the new coffee object
+    // Create the new movie object
     const newMovie = {
         title,
         rating,
@@ -51,16 +47,70 @@ const addMovie = (e) => {
 
 }
 
-const deleteMovie = () => {
-    return fetch('/api/movies')
-        .then(response => response.json());
+const deleteMovie = (id) => {
+    // e.preventDefault(); // don't submit the form, we just want to update the data
+    console.log(id);
+    const options = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+    fetch(`/api/movies/${id}`, options)
+        .then(() => {
+            console.log(`movie ${id} deleted`)
+        })
+        .catch(() => {
+            console.log('error on delete')
+        });
+
 }
-const updateMovie = () => {
-    return fetch('/api/movies')
-        .then(response => response.json());
+const updateMovie = (e) => {
+    e.preventDefault(); // don't submit the form, we just want to update the data
+    let rating = document.getElementById('new-rating').value;
+    let title = document.getElementById('new-name').value;
+    let id = document.getElementById("updateMovieID").value;
+
+    // Create the new movie object
+    const updateMovie = {
+        title,
+        rating,
+        id
+    };
+    console.log(updateMovie);
+    // Add the new movie to the array
+
+    //const blogPost = {title: 'Ajax Requests', body: 'Are a fun way to use JS!'};
+    const url = `/api/movies/${id}`;
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updateMovie),
+    };
+    fetch(url, options)
+        .then(/* post was created successfully */)
+        .catch(/* handle errors */);
 }
 
+const moveData = (id) => {
+    return fetch(`/api/movies/${id}`)
+        .then(response => response.json())
+        .then(movie => {
+            console.log(movie);
+            let {title, rating, id} = movie;
+            console.log(title);
+            document.getElementById("new-name").value = title;
+            document.getElementById("new-rating").value = rating;
+            document.getElementById("updateMovieID").value = id;
+
+        });
+}
+
+
 const displayMovies = () => {
+    let idArray = [];
     return fetch('/api/movies')
         .then(response => response.json())
         .then(movies => {
@@ -81,18 +131,38 @@ const displayMovies = () => {
                     starHTML += `<span class="fa fa-star"></span>`;
                 }
                 //starHTML +=          <span class="fa fa-star"></span>
-                console.log(starHTML);
+                //console.log(starHTML);
                 html += ` <div class="card-footer"> ${starHTML} </div>`;
-                html += `</div></div>`;
+                html += `</div>`;
+                html += `<div class="row">`;
+                html += `<div class="col-md-6"><button class="delete-movie btn btn-success btn-lg btn-block" id="delete${id}">Delete</button></div>`;
+                html += `<div class="col-md-6"><button class="delete-movie btn btn-success btn-lg btn-block" id="update${id}">Update</button></div>`;
+                html += `</div></div > `;
+                idArray.push(id);
             });
             console.log(html);
             document.getElementById('movie-list').innerHTML = html;
             document.getElementById('loading').style.display = 'none';
-            //return html;
+            for (let i = 0; i < idArray.length; i++) {
+                document.getElementById(`delete${idArray[i]}`).addEventListener('click', event => {
+                    //alert('Hello');
+                    console.log(idArray[i]);
+                    deleteMovie(idArray[i]);
+                    displayMovies();
+                });
+            }
+            for (let i = 0; i < idArray.length; i++) {
+                document.getElementById(`update${idArray[i]}`).addEventListener('click', event => {
+                    //alert('Hello');
+                    //console.log(event);
+                    moveData(idArray[i]);
+                    displayMovies();
+                });
+            }
         })
 
 
 }
 
 
-module.exports = {getMovies, addMovie, deleteMovie, updateMovie, displayMovies};
+module.exports = {getMovies, addMovie, deleteMovie, updateMovie, displayMovies, moveData};

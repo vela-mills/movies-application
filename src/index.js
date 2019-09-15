2e3fdf015741d9b512c6edd2abf345eacb6bb3a9
Gloria Laura Vela <gloria.laura.vela@gmail.com>
2019-09-15 08:21:27 -0500

    the spin loader will dislay after adding a movie, updating a movie or deleting a movie

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ const {getMovies, displayMovies, addMovie, updateMovie, saveSearchCriteria} = re
 /**Displays all movies in the console */
 
 getMovies().then((movies) => {
-   // console.log('Here are all the movies:');
+   console.log('Here are all the movies:');
     movies.forEach(({title, rating, id}) => {
-        //console.log(`id#${id} - ${title} - rating: ${rating}`);
+        console.log(`id#${id} - ${title} - rating: ${rating}`);
     });
 }).catch(() => {
     alert('Oh no! Something went wrong.\nCheck the console for details.');
